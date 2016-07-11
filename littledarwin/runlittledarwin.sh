e23313ed8de8ba075bee27e3aa230081d24df1ce
Ali Parsai <ali.parsai@email.com>
2016-07-11 18:52:38 +0200

    fixed 3

--- littledarwin/runlittledarwin.sh
+++ littledarwin/runlittledarwin.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-pypy ./LittleDarwin.py -m -b -t ../ -p ../src/main/ --timeout=180 -c mvn,test
+git status
+touch testfile
+git add testfile
+git commit -a -m "travis"
+git push
+
+#pypy ./LittleDarwin.py -m -b -t ../ -p ../src/main/ --timeout=180 -c mvn,test
 
